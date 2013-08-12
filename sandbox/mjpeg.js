73aa69f00cc86a107bdf8665e88975925e7d951c
Alice-MacAir <amchung@stanford.edu>
2013-08-12 12:49:51 -0700

    js files first update

---
+++ sandbox/mjpeg.js
@@ -0,0 +1,93 @@
+﻿//  Sample refactored by David Rousset - Microsoft France - http://blogs.msdn.com/davrous 
+//  Using Hand.js to support all platforms
+
+//  Based on https://github.com/sebleedelisle/JSTouchController/blob/master/Touches.html 
+
+//  Copyright (c)2010-2011, Seb Lee-Delisle, sebleedelisle.com. All rights reserved.
+
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+//  that the following conditions are met:
+
+//    * Redistributions of source code must retain the above copyright notice, 
+//      this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above copyright notice, 
+//      this list of conditions and the following disclaimer in the documentation 
+//      and/or other materials provided with the distribution.
+
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+//  AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+//  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+//  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+//  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+        window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+var canvas,
+c, // c is the canvas' context 2D
+container,
+halfWidth,
+halfHeight;
+
+/////////////////////////////// streaming
+
+document.addEventListener("DOMContentLoaded", init);
+	
+window.onorientationchange = resetCanvas;
+window.onresize = resetCanvas;
+
+function init() {
+    //requestAnimFrame(draw);
+    console.log("initialized");
+
+    draw();
+}
+
+function resetCanvas(e) {
+    // resize the canvas - but remember - this clears the canvas too. 
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    halfWidth = canvas.width / 2;
+    halfHeight = canvas.height / 2;
+
+    //make sure we scroll to the top left. 
+    window.scrollTo(0, 0);
+}
+
+function draw() {
+	setupCanvas();
+	
+	// clear
+	//c.clearRect(0, 0, canvas.width, canvas.height);
+	
+	// draw
+	var img = new Image();
+	img.onload = function() {
+    	c.drawImage(img, 0, 0);
+	};
+	img.src = "http://171.65.102.132:8070/?action=snapshot?t=" + new Date().getTime();
+    
+    // request new frame
+    requestAnimFrame(draw);
+}
+
+function setupCanvas() {
+    canvas = document.getElementById('canvasSurfaceGame');
+    c = canvas.getContext('2d');
+    resetCanvas();
+    c.strokeStyle = "#ffffff";
+    c.lineWidth = 2;
+}
